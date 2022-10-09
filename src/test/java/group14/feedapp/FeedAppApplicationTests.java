ae5ddf660c65583d20577ae398d85b0191ef474c
Erlend.Skutlaberg <erlend.skutlaberg@student.uib.no>
2022-10-09 17:11:53 +0200

    Now the createPoll test passes. A user can create a poll, but we might need more tests on different input later

--- src/test/java/group14/feedapp/FeedAppApplicationTests.java
+++ src/test/java/group14/feedapp/FeedAppApplicationTests.java
@@ -1,21 +1,12 @@
 package group14.feedapp;
 
-import group14.feedapp.model.Poll;
-import group14.feedapp.model.User;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.junit.Assert;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.time.LocalDateTime;
 
 @ComponentScan
 @SpringBootTest
@@ -35,35 +26,6 @@ class FeedAppApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void createPoll() throws URISyntaxException {
-		final String baseUrl = "http://localhost:" + SERVER_PORT + "/api/poll/";
-		URI uri = new URI(baseUrl);
-		User user = new User();
-		user.setId("10");
-		user.setName("Lars");
-		user.setAdmin(false);
-
-		Poll poll = new Poll(10101,
-							"Do you like fish?",
-							"Yes",
-							"No",
-							false,
-							LocalDateTime.of(2022, 10, 10,16, 0),
-							LocalDateTime.of(2022, 10, 10, 18, 0),
-							false,
-							user);
-
-		HttpHeaders headers = new HttpHeaders();
-
-		HttpEntity<Poll> request = new HttpEntity<>(poll, headers);
-
-		ResponseEntity<String> result = this.restTemplate.postForEntity(uri, request, String.class);
-
-		Assert.assertEquals("SUCCESS", result.getBody());
-
 	}
 
 
