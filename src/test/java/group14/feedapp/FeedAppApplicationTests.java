4fc33819c61d7c2ba3c07efc27f36d7531ce4210
Erlend.Skutlaberg <erlend.skutlaberg@student.uib.no>
2022-10-09 17:15:27 +0200

    Cleaned up the code

--- src/test/java/group14/feedapp/FeedAppApplicationTests.java
+++ src/test/java/group14/feedapp/FeedAppApplicationTests.java
@@ -1,28 +1,14 @@
 package group14.feedapp;
 
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.ComponentScan;
 
 
-@ComponentScan
+
 @SpringBootTest
 class FeedAppApplicationTests {
 
-
-
-	private TestRestTemplate restTemplate = new TestRestTemplate();
-
-	@LocalServerPort
-	final static String SERVER_PORT = "8080";
-
-	@BeforeAll
-	public static void startRESTServer() {
-		FeedAppApplication.main(new String[]{SERVER_PORT});
-	}
 
 	@Test
 	void contextLoads() {
